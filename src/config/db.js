6a819af934aa82d5f36dd11db8ef08c2a8053c3a
adejota <ademir.diamentejunior@gmail.com>
2021-01-31 15:54:30 -0300

    Configurações de conexão com banco mysql da AWS

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,14 @@
 var mysql = require('mysql')
 
 module.exports = mysql.createConnection({
-    user: "root",
+    /* user: "root",
     password: "Ad_914989",
     host: "127.0.0.1",
     port: 3306,
-    database: "receitas_pra_casinha"
+    database: "receitas_pra_casinha" */
+
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    host: process.env.RDS_HOSTNAME,
+    port: process.env.RDS_PORT
 })
