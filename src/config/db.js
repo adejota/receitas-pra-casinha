0cc1292f030d2dc5060b48a4137d5b2bf8c7eaa5
adejota <ademir.diamentejunior@gmail.com>
2021-02-01 10:26:57 -0300

    First express app

--- src/config/db.js
+++ src/config/db.js
@@ -12,6 +12,5 @@ module.exports = mysql.createConnection({
     user: process.env.RDS_USERNAME,
     password: process.env.RDS_PASSWORD,
     host: process.env.RDS_HOSTNAME,
-    port: process.env.RDS_PORT,
-    database: process.env.RDS_DB_NAME
+    port: process.env.RDS_PORT
 })
