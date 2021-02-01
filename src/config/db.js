2820afbb7713a0608b6f65b9c6383a0e0937b638
adejota <ademir.diamentejunior@gmail.com>
2021-02-01 14:35:46 -0300

    escondendo DB ambiente dev

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ var mysql = require('mysql')
 module.exports = mysql.createConnection({
     // Desenvolvimento
     /* user: "root",
-    password: "Ad_914989",
+    password: "********",
     host: "127.0.0.1",
     port: 3306,
     database: "receitas_pra_casinha" */
